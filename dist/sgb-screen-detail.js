0d57819ae9a832d20e764857ef51f709d43bc707
Saul Hernandez <sauldhernandez@gmail.com>
2015-04-24 17:59:12 -0430

    Added support for configurable title

--- dist/sgb-screen-detail.js
+++ dist/sgb-screen-detail.js
@@ -2,7 +2,8 @@
 'use strict';
 
 angular.module('sgb-screen-detail', ['megazord'])
-    .controller('sgb-screen-detail-controller', ['$stateParams', '$scope', function($stateParams, $scope){
+    .controller('sgb-screen-detail-controller', ['$stateParams','_screenParams', '$scope', function($stateParams, _screenParams, $scope){
+        $scope.title = _screenParams.title;
         $scope.item = $stateParams.data;
     }]);
 },{}]},{},[1]);
--- src/sgb-screen-detail.js
+++ src/sgb-screen-detail.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('sgb-screen-detail', ['megazord'])
-    .controller('sgb-screen-detail-controller', ['$stateParams', '$scope', function($stateParams, $scope){
+    .controller('sgb-screen-detail-controller', ['$stateParams','_screenParams', '$scope', function($stateParams, _screenParams, $scope){
+        $scope.title = _screenParams.title;
         $scope.item = $stateParams.data;
     }]);
