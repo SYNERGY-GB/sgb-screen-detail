34db09b9c1ed857074783f5ca6ce4ff50bdeb0de
luismiglietti <luismiglietti@gmail.com>
2015-07-22 14:19:35 -0430

    template refactored

--- src/sgb-screen-detail.js
+++ src/sgb-screen-detail.js
@@ -4,8 +4,6 @@ angular.module('sgb-screen-detail', ['megazord'])
     .controller('sgb-screen-detail-controller', ['$stateParams', '_screen', '_screenParams', '$scope', function ($stateParams, _screen, _screenParams, $scope) {
 
         _screen.initialize($scope, _screenParams);
-
         $scope.item = $stateParams.data;
-        $scope.res_location = _screenParams.res_location; 
 
     }]);
