29118c6f1633f2eeb5c7500e82d6530807b3443a
luismiglietti <luismiglietti@gmail.com>
2015-08-11 13:46:06 -0430

    changed screen to work with mz screen

--- dist/sgb-screen-detail.js
+++ dist/sgb-screen-detail.js
@@ -5,7 +5,12 @@ angular.module('sgb-screen-detail', ['megazord'])
     .controller('sgb-screen-detail-controller', ['$stateParams', '_screen', '_screenParams', '$scope', function ($stateParams, _screen, _screenParams, $scope) {
 
         _screen.initialize($scope, _screenParams);
-        $scope.item = $stateParams.data;
+        $scope.data = $stateParams.data;
+        $scope.templateType = _screenParams.templateType; 
 
-    }]);
+        //Delete after mz framework gets updated
+        $scope.screenType = "sgb-screen-detail";
+
+    }]); 
+
 },{}]},{},[1]);
