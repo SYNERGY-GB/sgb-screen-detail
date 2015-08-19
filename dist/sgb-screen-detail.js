337d37a79de0fa7b8310a157421fd2dd4fc01966
luismiglietti <luismiglietti@gmail.com>
2015-08-19 12:14:51 -0430

    refactored controller and templates

--- dist/sgb-screen-detail.js
+++ dist/sgb-screen-detail.js
@@ -2,10 +2,28 @@
 'use strict';
 
 angular.module('sgb-screen-detail', ['megazord'])
-    .controller('sgb-screen-detail-controller', ['$stateParams', '_screen', '_screenParams', '$scope', function ($stateParams, _screen, _screenParams, $scope) {
+    .controller('sgb-screen-detail-controller', ['_router', '$stateParams', '_screen', '_screenParams', '$scope', 
+                function (_router, $stateParams, _screen, _screenParams, $scope) {
 
         _screen.initialize($scope, _screenParams);
         $scope.data = $stateParams.data;
+
+/**************************************************/
+
+        $scope.goBack = function() {
+            _router.fireEvent(
+                {
+                 name: 'goBack', 
+                 params: {}
+                }
+            );  
+        }
+
+        $scope.extraParams = {
+            goBack: $scope.goBack
+        }
+
+
 
     }]); 
 
