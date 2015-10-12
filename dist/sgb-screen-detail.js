49db3390907d5c7e396887142b57477c6ab788f4
luismiglietti <10-10459@usb.ve>
2015-10-12 17:58:22 -0430

    changed stateparams for _data

--- dist/sgb-screen-detail.js
+++ dist/sgb-screen-detail.js
@@ -2,11 +2,11 @@
 'use strict';
 
 angular.module('sgb-screen-detail', ['megazord'])
-    .controller('sgb-screen-detail-controller', ['_router', '$stateParams', '_screen', '_screenParams', '$scope', 
-                function (_router, $stateParams, _screen, _screenParams, $scope) {
+    .controller('sgb-screen-detail-controller', ['_router', '_data', '_screen', '_screenParams', '$scope', 
+                function (_router, _data, _screen, _screenParams, $scope) {
 
         _screen.initialize($scope, _screenParams);
-        $scope.data = $stateParams.data;
+        $scope.data = _data;
 
 /**************************************************/
 
