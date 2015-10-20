b9c43beb984e9f3049a221e0fc95e7388bb120ae
Luis Miglietti <luismiglietti@gmail.com>
2015-10-20 18:05:22 -0430

    added back root navigation

--- src/sgb-screen-detail.js
+++ src/sgb-screen-detail.js
@@ -9,14 +9,17 @@ angular.module('sgb-screen-detail', ['megazord'])
 
 /**************************************************/
 
-        $scope.goBack = function(screens) {
-            $ionicHistory.goBack(screens); 
+        $scope.goBack = function() {
+            $ionicHistory.goBack(); 
+        }
+
+        $scope.goHome = function() {
+            $ionicHistory.goBack(($ionicHistory.viewHistory().histories.root.stack.length*(-1))-1);
         }
 
         $scope.extraParams = {
             goBack: $scope.goBack
         }
-
 
 
     }]); 
